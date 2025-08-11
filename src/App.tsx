2c7a12d736c7c744a5ecd6654016d7d0abf48409
codeee18 <lint4ngboy@gmail.com>
2025-08-11 14:47:43 +0700

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { Routes, Route } from "react-router-dom";
 import Home from './pages/home'
 import About from './pages/about'
