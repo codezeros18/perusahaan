927ac7cdd657379b1ce8375ca092246ce9c53797
codeee18 <lint4ngboy@gmail.com>
2025-08-13 04:01:18 +0700

    done

--- src/components/gallerycard.tsx
+++ src/components/gallerycard.tsx
@@ -50,8 +50,8 @@ export default function GalleryCard({ item }: GalleryCardProps) {
 
       {/* Desktop */}
 {/* Desktop */}
-<div className="absolute top-0 bottom-0 left-0 right-0 hidden md:grid place-items-center group pointer-events-none">
-  <div className="absolute top-0 bottom-0 left-0 right-0 bg-orange-500/80 opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-auto" />
+<div className="absolute inset-0 hidden md:grid place-items-center group pointer-events-none">
+  <div className="absolute inset-0 bg-orange-500/80 opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-auto" />
   <div className="border border-white p-4 text-white font-semibold text-center tracking-widest uppercase z-10 pointer-events-auto">
     {item.title}
   </div>
